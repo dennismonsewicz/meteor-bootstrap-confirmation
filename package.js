d60d90f1fba9e479dc3b20f53890f1bcc98b58df
Dennis Monsewicz <dmonsewicz@myemma.com>
2014-05-08 21:02:20 -0500

    bs3

--- package.js
+++ package.js
@@ -4,6 +4,7 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   api.use('jquery', 'client');
+  api.imply('jquery', 'client');
 
-  api.add_files('lib/bootstrap-confirmation.js', 'client');
+  api.add_files('lib/bootstrap3_confirmation.js', 'client');
 });
